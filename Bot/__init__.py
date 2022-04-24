b2b43977ac26a7f97fac466412e41367e51e23a7
Anshul Garg <garganshul553@gmail.com>
2022-04-25 03:27:43 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -2,10 +2,10 @@ from pyrogram import Client
 from os import getenv
 from dotenv import load_dotenv
 
-ID = getenv("ID")
-HASH = getenv("HASH")
-TOKEN = getenv("TOKEN")
-OWNER = getenv("OWNER")
+ID = 6328792 #getenv("ID")
+HASH = "8c3600d42da9c66da7ef7ea4f9763a53" #getenv("HASH")
+TOKEN = "5377908346:AAFU7rpT77UcDJBtUp8ov8CfDG6CquPx2k8" #getenv("TOKEN")
+#OWNER = getenv("OWNER")
 
 app = Client(
   "vot",
