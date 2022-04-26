a4ae84244116e575d370ca4f35a7495d2343a420
Anshul Garg <garganshul553@gmail.com>
2022-04-27 02:50:40 +0530

    Update and rename Bot/__main__.py to main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from Bot import app
-from .utils import Nocturnetls
-from pyrogram import filters
+from utils import Nocturnetls
+from pyrogram import filters, Client 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+ID = 6328792 #getenv("ID")
+HASH = "8c3600d42da9c66da7ef7ea4f9763a53" #getenv("HASH")
+TOKEN = "5377908346:AAFU7rpT77UcDJBtUp8ov8CfDG6CquPx2k8" #getenv("TOKEN")
+#OWNER = getenv("OWNER")
+
+app = Client(
+  "vot",
+  api_id=ID,
+  api_hash=HASH,
+  bot_token=TOKEN
+)
 
 
 @app.on_message(filters.command("start"))
@@ -14,22 +30,22 @@ async def novel(_, message):
   if query:
     sex = Nocturnetls(query)
     novel = sex.get_novel()
-    await message.reply_photo(
-      photo=novel['cover'],
-      caption=novel['name'],
+    await message.reply_text(
+      text=f"[{novel['name']}]({query})",
       reply_markup=InlineKeyboardMarkup(
         [
           [
             InlineKeyboardButton(
               text="Go To Novel",
-              url=novel['file']
+              url=novel['uri']
             )
           ]
         ]
-      )
+      ),
+      disable_web_page_preview=True  
     )
   else:
     await message.reply_text("Give url to chapter of Novel")
 
-if __name__ == "__main__":
-  app.run()
+
+app.run()
